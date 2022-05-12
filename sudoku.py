25b07dd7b5a8d56b12781795c964f3b5b3816949
Paulo Feresin <paulo.feresin@gmail.com>
2022-05-12 19:48:01 -0300

    board feito

--- sudoku.py
+++ sudoku.py
@@ -4,9 +4,12 @@ sudoku = []
 
 for linhas in range(9):
     linha = []
-    for colunas in range(9):
-        linha.append(int(input()))
+
+    for colunas in range(1):
+        entrada = input()
+        linha.append((entrada))
         
-    sudoku.append(linhas)
+    sudoku.append(linha)
 
 print(sudoku)
+
