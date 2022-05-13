cd4814f0a2d4d4c90e183a6fb7cf83050183699b
Paulo Feresin <paulo.feresin@gmail.com>
2022-05-12 22:27:31 -0300

    verificações feitas, precisa melhorar

--- sudoku.py
+++ sudoku.py
@@ -1,15 +1,81 @@
-## exercicio 1383 do beecrowd - impacta
+# exercicio 1383 do beecrowd - impacta
 
 sudoku = []
 
-for linhas in range(9):
-    linha = []
+n = int(input())
+teste = 0
+for i in range(0,n+1):
+    
+    print("Instancia", i)
+    for linhas in range(9):
+        linha = []
 
-    for colunas in range(1):
-        entrada = input()
-        linha.append((entrada))
-        
-    sudoku.append(linha)
+        for colunas in range(1):
+            entrada = input()
+            entrada = entrada.split()
+            linha.extend(entrada)
 
-print(sudoku)
+        sudoku.append(linha)
 
+    # print(sudoku)
+
+    colunas = []
+    cont = 0
+
+    for i in range(0, 9):
+        for linha in sudoku:
+            digito = linha[cont]
+            colunas.append(digito)
+            if len(set(linha)) != len(linha):
+                print("NAO")
+
+        cont += 1
+
+
+
+    for linha in sudoku:
+        if len(set(linha)) != len(linha):
+            print("NAO")
+
+
+# def testa_sudoku(n):
+#     for i in range(1,n+1):
+    
+#         print("Instancia", i)
+#         for linhas in range(9):
+#             linha = []
+
+#             for colunas in range(1):
+#                 entrada = input()
+#                 entrada = entrada.split()
+#                 linha.extend(entrada)
+
+#             sudoku.append(linha)
+
+#         # print(sudoku)
+
+#         colunas = []
+#         cont = 0
+
+#         for i in range(0, 9):
+#             for linha in sudoku:
+#                 digito = linha[cont]
+#                 colunas.append(digito)
+#                 if len(set(linha)) != len(linha):
+#                     print("NAO")
+#                 else:
+#                     print("SIM")
+#                     break
+
+#             cont += 1
+
+
+
+#         for linha in sudoku:
+#             if len(set(linha)) != len(linha):
+#                 print("NAO")
+#             else:
+#                 print("SIM")
+#                 break
+
+# testa_sudoku(n)
