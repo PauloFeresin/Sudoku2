637640476219a4732f6d74da7aacfcd184685633
Paulo Feresin <paulo.feresin@gmail.com>
2022-05-12 23:33:53 -0300

    verificações feitas, precisa melhorar

--- sudoku.py
+++ sudoku.py
@@ -4,38 +4,50 @@ sudoku = []
 
 n = int(input())
 teste = 0
-for i in range(0,n+1):
+# for i in range(0,n+1):
     
-    print("Instancia", i)
-    for linhas in range(9):
-        linha = []
+# print("Instancia", i)
+for linhas in range(9):
+    linha = []
 
-        for colunas in range(1):
-            entrada = input()
-            entrada = entrada.split()
-            linha.extend(entrada)
+    for colunas in range(1):
+        entrada = input()
+        entrada = entrada.split()
+        linha.extend(entrada)
 
-        sudoku.append(linha)
+    sudoku.append(linha)
 
-    # print(sudoku)
+# print(sudoku)
 
-    colunas = []
-    cont = 0
+colunas = []
+cont = 0
+falhas = 0
+acertos = 0
 
-    for i in range(0, 9):
-        for linha in sudoku:
-            digito = linha[cont]
-            colunas.append(digito)
-            if len(set(linha)) != len(linha):
-                print("NAO")
-
-        cont += 1
-
-
-
+for i in range(0, 9):
     for linha in sudoku:
+        digito = linha[cont]
+        colunas.append(digito)
         if len(set(linha)) != len(linha):
-            print("NAO")
+            falhas += 1
+        else:
+            acertos += 1
+
+    cont += 1
+
+
+
+for linha in sudoku:
+    if len(set(linha)) != len(linha):
+        falhas += 1
+    else:
+        acertos += 1
+
+
+if falhas > n:
+    print("NAO")
+if acertos > n:
+    print("SIM")
 
 
 # def testa_sudoku(n):
